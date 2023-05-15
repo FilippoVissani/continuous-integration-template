b596e14af04af5a5942302e259a3ffa88b5ffd7f
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-05-15 13:36:58 +0200

    ci: fix Semantic Release configuration

--- release.config.js
+++ release.config.js
@@ -3,7 +3,7 @@ const verifyReleaseCommands = `
 sed -i 's/version :=.*/version := "\${nextRelease.version}"/g' build.sbt || exit 1
 git add -A || exit 2
 git commit -m "chore: [skip ci] update version in build.sbt" || exit 3
-./sbtx publishSigned sonatypePrepare sonatypeBundleUpload sonatypeClose sonatypeDrop || exit 3
+sbt publishSigned sonatypePrepare sonatypeBundleUpload sonatypeClose sonatypeDrop || exit 3
 `
 const prepareCommands = `
 sed -i 's/version :=.*/version := "\${nextRelease.version}"/g' build.sbt || exit 1
@@ -12,9 +12,10 @@ git commit -m "chore: [skip ci] update version in build.sbt" || exit 3
 git push --force origin || exit 4
 `
 const publishCommands = `
+sbt assembly
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
 git push --force origin \${nextRelease.version} || exit 2
-./sbtx publishSigned sonatypeBundleRelease || exit 3
+sbt publishSigned sonatypeBundleRelease || exit 3
 `
 const releaseBranches = ["main"]
 config.branches = releaseBranches
