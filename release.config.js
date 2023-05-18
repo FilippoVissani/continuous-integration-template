7b7faa509435b6d1fb05de9bb0aa547810da7271
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-05-18 10:38:28 +0200

    feat: feat

--- release.config.js
+++ release.config.js
@@ -1,13 +1,14 @@
 const config = require('semantic-release-preconfigured-conventional-commits')
 const verifyReleaseCommands = `
+gpg --list-keys
 sed -i 's/version :=.*/version := "\${nextRelease.version}"/g' build.sbt || exit 1
-git add -A || exit 2
+git add build.sbt || exit 2
 git commit -m "chore: [skip ci] update version in build.sbt" || exit 3
 sbt publishSigned sonatypePrepare sonatypeBundleUpload sonatypeClose sonatypeDrop || exit 3
 `
 const prepareCommands = `
 sed -i 's/ThisBuild / version :=.*/ThisBuild / version := "\${nextRelease.version}"/g' build.sbt || exit 1
-git add -A || exit 2
+git add build.sbt || exit 2
 git commit -m "chore: [skip ci] update version in build.sbt" || exit 3
 git push --force origin || exit 4
 `
